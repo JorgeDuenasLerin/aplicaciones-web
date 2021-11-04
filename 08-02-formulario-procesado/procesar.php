3462cd59e7d8c080e0e36017919a76b2c4f20f82
jorge dueñas Lerín <jorge.duenas.lerin@gmail.com>
2021-11-04 13:45:49 +0100

    Ejemplo procesado form

---
+++ 08-02-formulario-procesado/procesar.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>FORM procesado</title>
+  </head>
+  <body>
+    <?php
+      print_r($_POST);       
+    ?>
+  </body>
+</html>
